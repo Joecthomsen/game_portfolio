62679e5a88dec32b3d0a2ac9fc44ac5582aeb2c0
joecthomsen <joecthomsen@gmail.com>
2022-08-21 19:05:24 +0200

    Igore ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -2,14 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
+
+// module.exports = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// }
 
 module.exports = nextConfig
